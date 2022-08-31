75af7b3fe042eb2a652e76056816dd5c082d5be6
Lauren <elsee388@protonmail.com>
2022-08-31 11:22:38 -0400

    Routes set and components attached, properly rendering and routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route, Link, Routes } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 
-import { Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News } from "./components";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
 import "./App.css";
 
 const App = () => {
@@ -14,23 +21,17 @@ const App = () => {
       <div className="main">
         <Layout>
           <div className="routes">
-            <Switch>
-              <Route exact path="/">
-                <Homepage />
-              </Route>
-              <Route exact path="/exchanges">
-                <Exchanges />
-              </Route>
-              <Route exact path="/cryptocurrencies">
-                <Cryptocurrencies />
-              </Route>
-              <Route exact path="/crypto/:coinId">
-                <CryptoDetails />
-              </Route>
-              <Route exact path="/news">
-                <News />
-              </Route>
-            </Switch>
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
           </div>
         </Layout>
       </div>
